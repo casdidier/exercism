1c1e2c05789580a6b413239d7af2ed152fd39dc0
didier.cheung <didier.cheung@contentsquare.com>
2023-02-14 23:55:44 +0100

    dart prob

---
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -0,0 +1,22 @@
+int score(String word) {
+  var letterValues = {
+    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+    'D': 2, 'G': 2,
+    'B': 3, 'C': 3, 'M': 3, 'P': 3,
+    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+    'K': 5,
+    'J': 8, 'X': 8,
+    'Q': 10, 'Z': 10,
+  };
+  
+  var score = 0;
+  for (var letter in word.toUpperCase().split('')) {
+    score += letterValues[letter] ?? 0;
+  }
+  return score;
+}
+
+void main() {
+  print(score('hello')); // prints 8
+  print(score('world')); // prints 9
+}
